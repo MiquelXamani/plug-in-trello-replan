51efe413ce54f4cd3e63b8ccef1a9264f2a89bfd
miquel.xamani <miquel.xamani@est.fib.upc.edu>
2017-02-27 14:05:53 +0100

    correcció userscontroller

--- src/main/java/web/controllers/UsersController.java
+++ src/main/java/web/controllers/UsersController.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import web.models.User;
+import web.repositories.UserRepository;
 
 @RestController
-@RequestMapping("/card")
+@RequestMapping("/users")
 public class UsersController {
+    UserRepository userRepository;
 
     @RequestMapping(method= RequestMethod.GET)
     public User getUserByUsername(String username){
-        return new User();
+        return userRepository.findByUsername(username);
     }
 }
--- src/main/java/web/repositories/UserRepository.java
+++ src/main/java/web/repositories/UserRepository.java
@@ -1,11 +1,10 @@
 package web.repositories;
 
-import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import web.models.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    List<User> findByUsername(String username);
+    User findByUsername(String username);
 }
