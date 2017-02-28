b99c29b058a939df18391fbe75cf3f44e654604e
miquel.xamani <miquel.xamani@est.fib.upc.edu>
2017-02-28 15:56:37 +0100

    obrir pop up auth Trello

--- src/main/webapp/js/register_controller.js
+++ src/main/webapp/js/register_controller.js
@@ -10,6 +10,8 @@
         var vm = this;
 
         vm.register = register;
+        vm.trelloAuth = false;
+        vm.authorizeIntegration = authorizeIntegration;
 
         function register() {
             vm.dataLoading = true;
@@ -27,6 +29,37 @@
                         //console.log("FAIL");
                     }
                 });
+        }
+
+        //Create popup window with Trello auth loaded.
+        function authorizeIntegration(){
+            //Trello API key
+            var key = "504327a0a1868e4f91dae5f6c852de79";
+            var authWindow, authUrl, token, trello, height, left, origin, receiveMessage, ref1, top, width;
+            width = 420;
+            height = 470;
+            left = window.screenX + (window.innerWidth - width) / 2;
+            top = window.screenY + (window.innerHeight - height) / 2;
+            origin = (ref1 = /^[a-z]+:\/\/[^\/]*/.exec(location)) != null ? ref1[0] : void 0;
+            //call_back=postMessage is necessary to enable cross-origin communication
+            authUrl = `https://trello.com/1/authorize?return_url=${origin}&callback_method=postMessage&expiration=never&name=Project&key=${key}`
+            authWindow = window.open(authUrl, 'trello', `width=${width},height=${height},left=${left},top=${top}`);
+            var receiveMessage = function(event) {
+                var ref2;
+                if ((ref2 = event.source) != null) {
+                    ref2.close();
+                }
+                if ((event.data != null) && /[0-9a-f]{64}/.test(event.data)) {
+                    //guardar el token a localstorage per recuperar-lo al moment de fer submit i borrar-lo després de localstorage
+                    console.log(event.data);
+                } else {
+                    token = null;
+                }
+                if (typeof window.removeEventListener === 'function') {
+                    window.removeEventListener('message', receiveMessage, false);
+                }
+            };
+            return typeof window.addEventListener === 'function' ? window.addEventListener('message', receiveMessage, false) : void 0;
         }
     }
 
