a7c3076c14bea3e79da9e1cc1ef06f61805711eb
miquel.xamani <miquel.xamani@est.fib.upc.edu>
2017-03-07 12:03:37 +0100

    crear un usuari per defecte quan arrenca el servidor

---
+++ src/main/java/web/InitialDataLoader.java
@@ -0,0 +1,25 @@
+package web;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import web.models.User;
+import web.repositories.UserRepository;
+
+@Component
+public class InitialDataLoader implements ApplicationRunner {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public InitialDataLoader(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void run(ApplicationArguments args) {
+        User user = new User("Miquel","123","b9af2c827b36369367e5416dcccb657a949f4745f1b41ee7f70d2fe91f78165e","miquelxamani1");
+        userRepository.save(user);
+    }
+}
