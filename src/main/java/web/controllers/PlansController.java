aaa2b49a07a63dd463f47bfa14c7b6acef103ee6
miquel.xamani <miquel.xamani@est.fib.upc.edu>
2017-03-14 13:39:16 +0100

    corregida la definició de job

--- src/main/java/web/controllers/PlansController.java
+++ src/main/java/web/controllers/PlansController.java
@@ -44,35 +44,44 @@ public class PlansController {
         p1.setCreated_at("27/02/2017");
         p1.setId(1);
         p1.setName("Release Març");
-        //Job1
+        //Feature 1
         Feature f1 = new Feature();
         f1.setId(1);
         f1.setName("Tasca 1");
         f1.setDescription("Aquesta serà la descripció de la tasca 1");
         f1.setEffort(1);
+        //Job 1
         Job j1 = new Job();
         j1.setEnds("23/03/2017");
         j1.setFeature(f1);
-        List<Resource> resourcesJ1 = new ArrayList<>();
-        resourcesJ1.add(r1);
-        j1.setResources(resourcesJ1);
-        //Job 2
+        j1.setResource(r1);
+        //Feature 2
         Feature f2 = new Feature();
         f2.setId(2);
         f2.setName("Tasca 2");
         f2.setDescription("Aquesta serà la descripció de la tasca 2");
         f2.setEffort(13);
+        //Job 2
         Job j2 = new Job();
         j2.setEnds("30/03/2017");
         j2.setFeature(f2);
-        List<Resource> resourcesJ2 = new ArrayList<>();
-        resourcesJ2.add(r2);
-        resourcesJ2.add(r3);
-        resourcesJ2.add(r4);
-        j2.setResources(resourcesJ2);
+        j2.setResource(r2);
+        //Job 3
+        Job j3 = new Job();
+        j3.setEnds("30/03/2017");
+        j3.setFeature(f2);
+        j3.setResource(r3);
+        //Job 4
+        Job j4 = new Job();
+        j4.setEnds("30/03/2017");
+        j4.setFeature(f2);
+        j4.setResource(r4);
+
         List<Job> jobsP1 = new ArrayList<>();
         jobsP1.add(j1);
         jobsP1.add(j2);
+        jobsP1.add(j3);
+        jobsP1.add(j4);
         p1.setJobs(jobsP1);
         plans[0] = p1;
 
@@ -81,42 +90,50 @@ public class PlansController {
         p2.setCreated_at("29/01/2017");
         p2.setId(2);
         p2.setName("Release Febrer");
+        //Feature 3
         Feature f3 = new Feature();
         f3.setId(3);
         f3.setName("Tasca 3");
         f3.setDescription("Aquesta serà la descripció de la tasca 3");
         f3.setEffort(5);
-        Job j3 = new Job();
-        j3.setEnds("23/03/2017");
-        j3.setFeature(f3);
-        List<Resource> resourcesJ3 = new ArrayList<>();
-        resourcesJ3.add(r1);
-        resourcesJ3.add(r4);
-        j3.setResources(resourcesJ3);
+        //Job 5
+        Job j5 = new Job();
+        j5.setEnds("23/03/2017");
+        j5.setFeature(f3);
+        j5.setResource(r1);
+        //Job 6
+        Job j6 = new Job();
+        j6.setEnds("23/03/2017");
+        j6.setFeature(f3);
+        j6.setResource(r4);
+
         List<Job> jobsP3 = new ArrayList<>();
-        jobsP3.add(j3);
+        jobsP3.add(j5);
+        jobsP3.add(j6);
         p2.setJobs(jobsP3);
         plans[1] = p2;
 
         return plans;
     }
 
-    /*@RequestMapping(value="/resources",method= RequestMethod.POST)
-    public ResponseEntity<Object> getUnlinkedResources(@RequestBody Plan plan){
-        String username = plan.getUsername();
+    @RequestMapping(value="/resources",method= RequestMethod.POST)
+    public List<Resource> getUnlinkedResources(@RequestBody PlanDTO planDTO){
+        String username = planDTO.getUsername();
         User user = userRepository.findByUsername(username);
         Long userId = user.getUserId();
         List<String> resourcesIds = new ArrayList<>();
-        for(int i = 0; i < plan.getResources().size(); i++){
-            resourcesIds.add(plan.getResources().get(i).getId());
+        List<Job> jobs = planDTO.getJobs();
+        for (Job j:jobs) {
+
         }
+
+
         Collections.sort(resourcesIds);
         List<ResourceMember> resourcesFound = resourceMemberRepository.findByUserIdAndResourceIdIn(userId,resourcesIds);
 
         List<Resource> notFoundMembers = new ArrayList<>();
         //falta trobar els que no estan assignats
-        return new ResponseEntity<>(user, HttpStatus.OK);
-    }*/
+    }
 
 
 }
--- src/main/java/web/models/Job.java
+++ src/main/java/web/models/Job.java
@@ -8,12 +8,11 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Job {
     private String ends;
-    private List<Resource> resources;
+    private Resource resource;
     private Feature feature;
     private List<Job> depends_on;
 
     public Job(){
-        this.resources = new ArrayList<>();
         this.depends_on = new ArrayList<>();
     }
 
@@ -25,12 +24,12 @@ public class Job {
         this.ends = ends;
     }
 
-    public List<Resource> getResources() {
-        return resources;
+    public Resource getResource() {
+        return resource;
     }
 
-    public void setResources(List<Resource> resources) {
-        this.resources = resources;
+    public void setResource(Resource resource) {
+        this.resource = resource;
     }
 
     public Feature getFeature() {
